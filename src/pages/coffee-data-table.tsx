f1ffb2262e292f7f39e36714de662da7d7445e45
Matthew Tapps <mail@matthewtapps.com>
2024-06-04 23:04:46 +1000

    add testing logging

--- src/pages/coffee-data-table.tsx
+++ src/pages/coffee-data-table.tsx
@@ -18,6 +18,7 @@ export default function CoffeeDataTablePage() {
     return () => window.removeEventListener("resize", updateMedia);
   });
 
+  console.log(data);
   return (
     <BaseContainer route="shotTable">
       {isLoading ? (
