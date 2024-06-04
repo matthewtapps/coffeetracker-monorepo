1f4bdd78a502f0b978dd4dc95c583b7590c9d189
Matthew Tapps <mail@matthewtapps.com>
2024-06-04 23:33:39 +1000

    add dashboard api query

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,13 @@
 import { BaseContainer } from "@/components/base-container";
 import { Charts } from "@/components/dashboard/charts";
-import { getDummyData } from "@/test/dummyData";
+import { useGetShotsQuery } from "@/app/api/api";
+import Spinner from "@/components/loadingSpinner";
 
 export default function DashboardPage() {
+  const { data, isLoading, isSuccess } = useGetShotsQuery();
   return (
     <BaseContainer route="dashboard">
-      <Charts data={getDummyData()} />
+      {isLoading ? <Spinner /> : isSuccess && <Charts data={data} />}
     </BaseContainer>
   );
 }
