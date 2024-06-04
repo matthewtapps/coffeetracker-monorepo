a0d7bcba6b1b948aaf41af83e7d1c3b8b9ad0f71
Matthew Tapps <mail@matthewtapps.com>
2024-06-04 23:05:59 +1000

    fix lambda permissions

--- deploy/10-lambda-permissions.tf
+++ deploy/10-lambda-permissions.tf
@@ -6,8 +6,8 @@ resource "aws_lambda_permission" "lambda_permission_get-all" {
   source_arn    = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
 
-resource "aws_lambda_permission" "lambda_permission_create_todo" {
-  statement_id  = "AllowExecutionFromAPIGatewayUCI"
+resource "aws_lambda_permission" "lambda_permission_post" {
+  statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.post.function_name
   principal     = "apigateway.amazonaws.com"
@@ -15,9 +15,17 @@ resource "aws_lambda_permission" "lambda_permission_create_todo" {
 }
 
 resource "aws_lambda_permission" "lambda_permission_get-latest" {
-  statement_id  = "AllowExecutionFromAPIGatewayUCI"
+  statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.get-latest.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "lambda_permission_cors-allow" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cors-allow.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
